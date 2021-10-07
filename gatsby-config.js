bac469ebdcd11164955a37139c066768d639b779
Jeff Lee <jefflee.ldh@gmail.com>
2021-10-07 17:18:27 -0400

    first push blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "upwind",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-theme-i18n`,
+      options: {
+        defaultLang: `en`,
+        prefixDefault: true,
+        configPath: require.resolve(`./i18n/config.json`),
+      },
+    },
+  ],
 };
